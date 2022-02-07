ec5d53343518f799b9c7f4862b0759979b9eec5c
Pedro Mengarelli <pedroam92@gmail.com>
2022-02-07 18:53:50 -0300

    avanceAPI

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
-var mysql = require('mysql');
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
 
-var conexion = mysql.createConnection({
+app.use(bodyParser.json());
+
+const connection = mysql.createConnection({
     host: "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com",
     user: "bsale_test",
     password: "bsale_test",
     database: "bsale_test"
 });
 
-conexion.connect(function(error){
-    if(error){
-        throw error;
-    }else{
-        console.log('Conexión Exitosa!');
+
+app.get('/', (req, res) => {
+    res.send('Bienvenido a la API! :D')
+})
+
+app.get('/productos', (req, res) => {
+    res.send('Lista de productos!')
+})
+
+app.post('/add', (req, res) => {
+    res.send('Nuevo prodcuto')
+})
+
+app.put('/update/:id', (req, res) => {
+    res.send('Actualizar producto por id')
+})
+
+app.delete('/delete/:id', (req, res) => {
+    res.send('Borrar producto')
+})
+
+//check connection
+connection.connect(function(error){
+    if(error) throw error;
+    {
+        console.log('Estamos conectados a la base de datos!:)');
     }
 });
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
 //query de la BD
-conexion.query('SELECT * from ...', function(error, results, fields){
-    if(error)
-    throw error;
+//connection.query('SELECT * from users', function(error, results){
+//    if(error) throw error;
 
-    results.forEach(results => {
-        console.log(results);
-    });
-});
+//    results.forEach(results => {
+//        console.log(results);
+//    });
+//});
 
-conexion.end();
+connection.end();
